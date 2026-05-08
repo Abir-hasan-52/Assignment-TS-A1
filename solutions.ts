aed947c56714a3cd7ec25295451ad29864889f25
Abir Hasan <abirhasan5208@gmail.com>
2026-05-08 17:59:45 +0600

    add: solution 2

--- solutions.ts
+++ solutions.ts
@@ -5,6 +5,15 @@ const filterEvenNumbers = (num: number[]): number[] => {
   return num.filter((num) => num % 2 === 0);
 };
 const result = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
-console.log(result);
+// console.log(result);
 
 
+//Problem2: Write a function reverseString that takes a string as input and returns the reversed version of that string.
+
+
+const reverseString = (input: string): string => {
+  return input.split("").reverse().join("");
+};
+
+console.log(reverseString("typescript"));
+ 
